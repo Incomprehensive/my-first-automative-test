3cefe25b986de0246f2a0fe5a23218497401d89d
Incomprehensive <noaccount51009@outlook.com>
2017-06-28 23:59:45 +0300

    Update MyFirstTest.java

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,25 +1,53 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import java.lang.String;
 
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Anton Pelianski on 28.06.2017.
  */
 public class MyFirstTest {
-    @Test
-   public void newGoogleSearch() throws InterruptedException {
-
+    private WebDriver driver;
+    public void beforeClass() {
         System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\RemoteDriver\\Chrome\\chromedriver.exe"); // Declare a path to remote Chrome driver
-        WebDriver driver = new ChromeDriver(); // Open a Chrome Browser page
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS); //Setting uo Implicid Wait
-        driver.get("https://google.com.ua"); // Open a Google website
-        driver.findElement(By.id("lst-ib")).sendKeys("The Infestation"); // Locate a text area and type in search inqury
-        driver.findElement(By.id("_fZl")).click(); // Initiate search
-        driver.findElement(By.xpath("//div[@id='rso']//a[contains(text(), 'THE INFESTATION')]")).click(); //Find a link to a band group
+        driver = new ChromeDriver(); // Open a Chrome Browser page
+    }
+    public void afterClass() {
         driver.close(); //close the Browser
+    }
+    public void delay(int var){
+        driver.manage().timeouts().implicitlyWait(var, TimeUnit.MILLISECONDS); //Setting uo Implicid Wait
+    }
+    @FindBy (id = "lst-ib")
+    public WebElement searchField;
 
-}
+    @FindBy (id = "_fZl")
+    public WebElement searchButton;
+
+    @FindBy (xpath = "//div[@id='rso']//a[contains(text(), 'THE INFESTATION')]")
+    public WebElement searchResult;
+
+    public void search() {
+        searchField.sendKeys("querry");
+        searchButton.click();
+    }
+
+    @Test
+    public void newGoogleSearch() throws InterruptedException {
+        beforeClass();
+        delay(50);
+        driver.get("https://google.com.ua"); // Open a Google website
+        //search("The Infestation"); //Locate text area nad type in search querry
+        searchButton.click(); // Initiate search
+        searchResult.click(); //Find a link to a band group
+        afterClass();
+    }
 }
