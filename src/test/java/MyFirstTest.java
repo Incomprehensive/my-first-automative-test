0f8d42a21987a6fd571c87afdefb3f21f5a1fcd3
Incomprehensive <noaccount51009@outlook.com>
2017-06-28 14:31:36 +0300

    Update MyFirstTest.java
    Added search via xpath

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -3,23 +3,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Anton Pelianski on 28.06.2017.
  */
 public class MyFirstTest {
     @Test
+   public void newGoogleSearch() throws InterruptedException {
 
-   public void GoogleSearch() throws InterruptedException {
-
-        final String s = System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\RemoteDriver\\Chrome\\chromedriver.exe"); // Declare a path to remote Chrome driver
+        System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\RemoteDriver\\Chrome\\chromedriver.exe"); // Declare a path to remote Chrome driver
         WebDriver driver = new ChromeDriver(); // Open a Chrome Browser page
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS); //Setting uo Implicid Wait
         driver.get("https://google.com.ua"); // Open a Google website
         driver.findElement(By.id("lst-ib")).sendKeys("The Infestation"); // Locate a text area and type in search inqury
         driver.findElement(By.id("_fZl")).click(); // Initiate search
-        Thread.sleep (4000);
-        System.out.print("Please Wait");
-        Thread.sleep(4000);
-        driver.findElement(By.partialLinkText("THE INFESTATION")).click(); //Find a link to a band group
+        driver.findElement(By.xpath(".//*[@id='rso']/div/div/div[4]/div/div/h3/a")).click(); //Find a link to a band group
         driver.close(); //close the Browser
 
 }
