c7785b34cecb5741b35708fe7cb230c43e74a663
Incomprehensive <noaccount51009@outlook.com>
2017-06-29 01:14:30 +0300

    Update MyFirstTest.java
    Added init method

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,14 +1,11 @@
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
-import java.lang.String;
+import org.openqa.selenium.support.PageFactory;
 
-import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -26,6 +23,7 @@ public class MyFirstTest {
     public void delay(int var){
         driver.manage().timeouts().implicitlyWait(var, TimeUnit.SECONDS); //Setting uo Implicid Wait
     }
+
     @FindBy (id = "lst-ib")
     public WebElement searchField;
 
@@ -35,18 +33,22 @@ public class MyFirstTest {
     @FindBy (xpath = "//div[@id='rso']//a[contains(text(), 'THE INFESTATION')]")
     public WebElement searchResult;
 
-    public void search() {
-        searchField.sendKeys("querry");
+    public void search(String query) {
+        searchField.sendKeys(query);
         searchButton.click();
+    }
+
+    public void init(final WebDriver driver){
+        PageFactory.initElements(driver, this);
     }
 
     @Test
     public void newGoogleSearch() throws InterruptedException {
+        init(driver);
         beforeClass();
         delay(10);
         driver.get("https://google.com.ua"); // Open a Google website
-        search("The Infestation"); //Locate text area nad type in search querry
-        searchButton.click(); // Initiate search
+        search("THE INFESTATION"); //Locate text area and type in search query
         searchResult.click(); //Find a link to a band group
         afterClass();
     }
