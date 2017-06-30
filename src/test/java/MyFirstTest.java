1f5cc448a9b622870a64154f833d68070c823aa7
Incomprehensive <noaccount51009@outlook.com>
2017-06-30 17:21:16 +0300

    Update MyFirstTest.java
    Supervisor revision

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -3,49 +3,62 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by Anton Pelianski on 28.06.2017.
+ * Created by Anton Pelianski on 30.06.2017.
  */
-public class MyFirstTest {
-    private String expectedTitle = "Google";
+public class SeleniumTest {
+    @FindBy (xpath = "//span[@class = 'a-like bold']")
+    public WebElement loginBtn1;
 
-    @FindBy (xpath = ".//*[@id='lst-ib']")
-    public WebElement searchField;
+    @FindBy (id = "top-login-uname")
+    public WebElement username;
 
-    @FindBy (id = "_fZl")
-    public WebElement searchButton;
+    @FindBy (id = "top-login-pwd")
+    public WebElement pass;
 
-    @FindBy (xpath = "//div[@id='rso']//a[contains(text(), 'THE INFESTATION')]")
-    public WebElement searchResult;
+    @FindBy (xpath = "//div[@class = 'logged-in-as-wrap']/a[@href]")
+    public WebElement logoutBtn;
+
+    @FindBy (id="top-login-btn")
+    public WebElement loginBtn2;
+
+    @FindBy (className = "logged-in-as-cap")
+    public WebElement verify;
+
+    private String user = "kneeninja1";
+    private String password = "IY7DP";
 
     private WebDriver driver;
 
     @Before
-    public void beforeClass() {
-        System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\RemoteDriver\\Chrome\\chromedriver.exe"); // Declare a path to remote Chrome driver
-        driver = new ChromeDriver(); // Open a Chrome Browser page
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //Setting uo Implicit Wait
+    public void initialize()
+    {
+        System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\RemoteDriver\\Chrome\\chromedriver.exe");
+        driver = new ChromeDriver();
         PageFactory.initElements(driver, this);
     }
     @Test
-    public void newGoogleSearch() throws InterruptedException {
-        driver.get("https://google.com.ua"); // Open a Google website
-        String actualTitle = driver.getTitle();
-        Assert.assertEquals(expectedTitle, actualTitle);
-        searchField.sendKeys("THE INFESTATION"); //Locate text area and type in search query
-        searchButton.click();
-        searchResult.click(); //Find a link to a band group
-    }
+    public void testLogin() {
+        WebDriverWait hold =  new WebDriverWait(driver, 10);
+        driver.get("https://rutracker.org");
+        loginBtn1.click();
+        username.sendKeys(user);
+        pass.sendKeys(password);
+        loginBtn2.click();
+        hold.until(ExpectedConditions.elementToBeClickable(logoutBtn));
+        Assert.assertEquals(user, verify.getText());
+        logoutBtn.click();
+        }
     @After
-    public void afterClass() {
-
-        driver.close(); //close the Browser
+    public void close(){
+        driver.close();
     }
-
 }
